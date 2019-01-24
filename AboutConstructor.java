6b691c572ef5f24664e79d5c25bc4944cf45620d
Nihar3011 <patelnihar1997.pn@gmail.com>
2019-01-24 16:45:37 +0530

    Basic java programs

---
+++ AboutConstructor.java
@@ -0,0 +1,42 @@
+
+package AboutConstructor;
+
+public class AboutConstructor {
+  private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+    private AboutConstructor(){ setA(10); }
+    
+     private static AboutConstructor temp = null; 
+ 
+  static AboutConstructor UseConstructor(){
+     if(temp==null)
+      temp= new AboutConstructor();
+      return temp;
+    }
+  void display(){
+      System.out.println( getA());
+  }
+    
+}
+class run{
+    public static void main(String args[]){ 
+       
+     AboutConstructor obj= AboutConstructor.UseConstructor();
+      AboutConstructor obj1= AboutConstructor.UseConstructor();  
+     System.out.println(obj);
+          System.out.println(obj1);
+          obj.setA(20);
+     obj.display();
+     obj1.display();
+                   
+     System.gc();
+      Runtime.getRuntime().gc();
+    }
+}
